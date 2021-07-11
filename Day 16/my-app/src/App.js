3cf50537dfded1abe8a55bae89722176ed06d693
Atisheel-Yadav <atisheelyadav@gmail.com>
2021-07-11 11:38:25 -0700

    Day 16 Added

---
+++ Day 16/my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+ 
+function App(){
+    return(
+     <div>
+         <div className='container'>
+     <img src="https://images-fe.ssl-images-amazon.com/images/I/61vDpgogunL.jpg">
+
+     </img>
+
+     <h1>Meme Card</h1>
+     </div>
+     </div>
+    )
+}
+export default App
