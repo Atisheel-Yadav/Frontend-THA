6665e149f6f111d9439aec2f08a50931c9cd8df4
Atisheel-Yadav <atisheelyadav@gmail.com>
2021-07-17 13:14:09 -0700

    Adding tha  19

---
+++ Day 19/my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from "./components/Header"
+import Button from "./components/Button"
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Button />
+    <Button />
+    <Button />
+    <Button />
+    </>
+  )
+}
+
+export default App;
