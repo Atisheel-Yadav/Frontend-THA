751323b5b92b5a7cd804db7b23f14b38c691c61f
Atisheel-Yadav <atisheelyadav@gmail.com>
2021-08-17 15:58:33 -0700

    Added tha 25

---
+++ DAY25/my-app/src/App.js
@@ -0,0 +1,40 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <>
+    <Card style={{ width: '18rem',backgroundColor:'lightskyblue',margin:'2rem 2rem' }}>
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
+
+    <Card style={{ width: '18rem',backgroundColor:'lightgreen',margin:'2rem 2rem' }}>
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="danger">Click here</Button>
+  </Card.Body>
+</Card>
+</>
+
+
+  );
+}
+
+export default App;
